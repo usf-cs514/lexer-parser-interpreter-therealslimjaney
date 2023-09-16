2c83f968ba86a5fe9cd0c9c1841c03ff1f76f744
Jane Swingler <jswingler007@gmail.com>
2023-09-15 20:43:15 -0700

    Third parser commit

--- src/Parser.java
+++ src/Parser.java
@@ -1,7 +1,18 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+
+// Parser: Determines if a statement or statements is valid based on the expected structure of those statements, and displays an error if not.
+
+/** BNF
+ * <assignment-stmt> : <identifier> = <arithmetic-expr>
+ *     <arithmetic-expr> : <term> | <arithmetic-expr> + <term>
+ *         <term>: <identifier> | <integer>
+ */
+
+// Use a recursive descent parsing scheme: define a ParseX method for each particular “part of speech”.
+// For example, parseAssignOp will be called when you are expecting a token of type “AssignOp”.
 public class Parser {
-    ArrayList<Token> tokenList;;
+    ArrayList<Token> tokenList;
+    int listIndex=0;
 
     //TODO implement Parser class here
     public Parser(){
@@ -10,23 +21,44 @@ public class Parser {
         ArrayList<Token> tokenList = lexer.getAllTokens();
     }
 
+    // Jane! Note, there are assignment statements
+    // assignment statement has structure: LHS=RHS
+    // Assignment operators:
+    // Expression: RHS
+
+
     private void parseProgram() {
         //this method drives the process and parses an entire program.
         // This method should call parseAssignment within a loop.
-        parseAssignment();
+        while (true) {
+            parseAssignment();
+        }
     }
 
     private void parseAssignment() {
-        // this method should parse a single assignment statement
+        // this method should parse a single assignment statement (LHS=RHS)
         // it should call parseId, parseAssignmentOp, and parseExpression.
     }
 
-    private void parseId() {
-        // this method parses a single identifier
+    // This method parses a single identifier
+    private boolean parseId() {
+        if (nextToken().type == "ID") {
+            return true;
+        } else {
+            System.out.println("Expecting identifier");
+        }
+        return false;
     }
 
+    //this method parses a single assignment operator
     private void parseAssignOp() {
-        //this method parses a single assignment operator
+            if (nextToken().type == "ASSMT") {
+                return true;
+            } else {
+                System.out.println("Expecting assignment operator");
+            }
+            return false;
+        }
     }
 
     private void parseExpression() {
@@ -34,8 +66,10 @@ public class Parser {
         // Note that expressions can include an unlimited number of “+” signs, e.g., “Y+3+4”
     }
 
-    private void nextToken() {
-        //this method gets the next token in the list and increments the index.
+    //this method gets the next token in the list and increments the index.
+    private Token nextToken() {
+        listIndex++;
+        return tokenList.get(listIndex);
     }
 
     private String toString() {
